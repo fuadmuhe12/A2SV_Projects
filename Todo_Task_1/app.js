d8a62001bb3e1fe714f19ffece66ae7bd74d0126
fuadmuhe12 <fuad.mohamed@aait.edu.et>
2024-08-06 14:54:24 +0300

    updated the code to support Edit

--- Todo_Task_1/app.js
+++ Todo_Task_1/app.js
@@ -10,15 +10,24 @@ addBtn.addEventListener('click', () => {
         <li>
             <span>${textArea.value}</span>
             <button id="del${root.childNodes.length + 1}">Delete</button>
+            <button id="edit${root.childNodes.length + 1}">Edit</button>
+
         </li>`
-        var curId = `del${root.childNodes.length + 1}`
+        var delbtn = `del${root.childNodes.length + 1}`
+        var editBtn = `edit${root.childNodes.length + 1}`
         textArea.value = ''
 
 
         root.innerHTML += newTask;
-        var delbtn = document.getElementById(curId).addEventListener('click', (e) => {
+        var delbtn = document.getElementById(delbtn).addEventListener('click', (e) => {
             e.target.parentElement.remove();
         })
+
+        var editElemet = document.getElementById(editBtn)
+        editElemet.addEventListener('click', (e) => {
+            textArea.value = e.target.parentElement.querySelector("span").textContent;
+            e.target.parentElement.remove();
+        });
     }
 });
 
